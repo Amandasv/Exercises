64389f0ec0a50bcae1099dbade2646b537ae198c
Amanda Vieira <amandasv.pm@gmail.com>
2024-02-27 16:33:58 -0300

    add calculator

--- tip-calculator/src/App.tsx
+++ tip-calculator/src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {FormControl, InputLabel, Input, InputAdornment, Card, CardContent, CardActions, Grid} from '@mui/material';
+import PeopleIcon from '@mui/icons-material/People';
 
-function App() {
+
+export default function App() {
+  const [bill, setBill] = useState(50);
+  const [tipPercentage, setTipPercentage] = useState(18);
+  const [people, setPeople] = useState(1);
+
+  const totalTip = (bill * tipPercentage) / 100;
+  const perPersonTip = totalTip/people ;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={3}>
+        <Card sx={{ maxWidth: 300, flexGrow: 1,  }} >
+        <CardContent>
+          <FormControl sx={{ m: 1 }} variant="standard">
+            <InputLabel htmlFor="bill">Total Bill</InputLabel>
+            <Input
+              id="bill"
+              value={isNaN(bill)? 0 : bill}
+              onChange={event => {setBill(parseInt(event.target.value))}}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            />
+          </FormControl>
+
+          <FormControl  sx={{ m: 1 }} variant="standard">
+            <InputLabel htmlFor="tipPercentage">Tip Percentage</InputLabel>
+            <Input
+              id="tipPercentage"
+              value={isNaN(tipPercentage)? 0 : tipPercentage}
+              onChange={event => {setTipPercentage(parseInt(event.target.value))}}
+              startAdornment={<InputAdornment position="start">%</InputAdornment>}
+            />
+          </FormControl>
+          <FormControl  sx={{ m: 1 }} variant="standard">
+            <InputLabel htmlFor="people">
+            Number of People
+            </InputLabel>
+            <Input
+              id="people"
+              value={isNaN(people)? 0 : people}
+              onChange={event => {setPeople(parseInt(event.target.value))}}
+              startAdornment={
+                <InputAdornment position="start">
+                  <PeopleIcon />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </CardContent>
+        <CardActions>
+          <p>Total Tip: {isNaN(totalTip)? '-' : `$${totalTip.toFixed(2)}`}</p>
+          <p>Tip Per Person: {isNaN(perPersonTip) ? '-' : `$${perPersonTip.toFixed(2)}`}</p>
+        </CardActions>
+      </Card>
+        </Grid>
+      </Grid>
+
+    </>
+    
   );
 }
-
-export default App;
